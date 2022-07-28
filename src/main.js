68a1b06eb909ace9e6cef8a662b71edd305f587c
Carlos Blanco <carlosblanco@me.com>
2022-07-28 23:27:33 +0200

    Testing API call

--- src/main.js
+++ src/main.js
@@ -3,3 +3,11 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+const URL = 'https://imdb-api.com/en/API/Search/k_79orqyvt/Rear Window';
+
+fetch(URL)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+  });
