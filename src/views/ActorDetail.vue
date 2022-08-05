6802b0f57359290a3bd664770329119d8f624f46
Carlos Blanco <carlosblanco@me.com>
2022-08-05 15:39:41 +0200

    Movie list style

--- src/views/ActorDetail.vue
+++ src/views/ActorDetail.vue
@@ -27,14 +27,22 @@ export default {
 
 <template>
   <router-link :to="{ name: 'MovieDetail', params: { movieid: movieid } }" class="back-button"></router-link>
-  <img :src="actor.image" :alt="actor.name">
-  <p>{{ actor.name }}</p>
-  <p>{{ actor.summary }}</p>
-  <div v-for="movie in actor.knownFor">
-    <router-link :to="{ name: 'MovieDetail', params: { movieid: movie.id } }">
-      <p>{{ movie.title }}</p>
-    </router-link>
+  <div class="actor-profile">
+    <img :src="actor.image" :alt="actor.name">
+    <p>{{ actor.name }}</p>
   </div>
+  <article>
+    <p>{{ actor.summary }}</p>
+    <h3>Known For Movies</h3>
+    <div class="knownfor-movies">
+      <div v-for="movie in actor.knownFor">
+        <router-link :to="{ name: 'MovieDetail', params: { movieid: movie.id } }">
+          <img :src="movie.image" :alt="movie.title">
+          <p>{{ movie.title }}</p>
+        </router-link>
+      </div>
+    </div>
+  </article>
 </template>
 
 <style scoped lang="scss">
@@ -47,5 +55,61 @@ export default {
   background: url(/src/assets/images/icons/arrow-left.svg) no-repeat;
   background-size: contain;
   z-index: 2;
+}
+.knownfor-movies {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  flex-direction: row;
+  div {
+    flex: 1;
+  }
+  img {
+    width: 150px;
+    height: 222px;
+    object-fit: cover;
+    display: block;
+    margin: 1rem auto;
+  }
+  p {
+    text-align: center;
+    font-size: 1rem;
+    font-weight: bold;
+    margin: 0.5rem auto;
+  }
+}
+article {
+  padding: 20px;
+  p {
+    font-size: 0.8rem;
+    margin: 5px;
+  }
+  h3 {
+    color: #fff;
+    font-variation-settings: 'wght' 600;
+    margin: 1rem 0 0.5rem;
+  }
+  img {
+    height: auto;
+    object-fit: cover;
+    display: block;
+  }
+}
+.actor-profile {
+  margin-top: 2rem;
+  img {
+    width: 150px;
+    height: 150px;
+    object-fit: cover;
+    border-radius: 50%;
+    display: block;
+    margin: 0 auto;
+  }
+
+  p {
+    text-align: center;
+    font-size: 1rem;
+    text-align: center;
+  }
 }
 </style>
