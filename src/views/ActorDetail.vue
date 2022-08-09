cfccb34cedce7ddf41dfbbea3b114a9471dd09ff
Carlos Blanco <carlosblanco@me.com>
2022-08-09 16:42:02 +0200

    Add the moviedb api

--- src/views/ActorDetail.vue
+++ src/views/ActorDetail.vue
@@ -1,6 +1,6 @@
 <script>
-const API_KEY = 'k_79orqyvt';
-const API_URL = `https://imdb-api.com/en/API`;
+const API_KEY = '17cdde2817ad9091721cd65fdeb37d58';
+const API_URL = 'https://api.themoviedb.org/3/';
 export default {
   name: "ActorDetail",
   props: ["actorid", "movieid"],
