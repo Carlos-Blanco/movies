431c182a5d8a0c14a8bd19ec466a82db529ac7df
Carlos Blanco <carlosblanco@me.com>
2023-06-12 15:03:10 +0200

    Add view

---
+++ src/views/UserMovies.vue
@@ -0,0 +1,17 @@
+<script>
+const API_KEY = '17cdde2817ad9091721cd65fdeb37d58';
+const API_URL = 'https://api.themoviedb.org/3/';
+export default {
+  name: "UserMovies",
+  data() {
+  }
+}
+</script>
+
+<template>
+ 
+</template>
+
+<style scoped lang="scss">
+
+</style>
